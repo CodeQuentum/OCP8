a10f1872c6f0dde7c708c02662f59a8aef131618
Quentin Nicot <q.nicot@icloud.com>
2023-09-14 17:47:37 +0200

    tout les composant de la page d'accueil sont la

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../src/components/Header'; 
 import HomeContent from './components/HomeContent';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header /> 
       <HomeContent />
+      <Footer />
     </div>
   );
 }
--- src/components/ProjectGallery.js
+++ src/components/ProjectGallery.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import projectData from '../data/projects.json'; // Importez le fichier JSON
+import projectData from '../data/projects.json';
+import '../styles/ProjectsGallery.css';
 
 function ProjectGallery() {
   return (
     <div className="project-gallery">
         <h2>Mes projets</h2>
-      <ul>
+      <ul className='gallery'>
         {projectData.map((project) => (
           <li key={project.id}>
             <img src={project.cover} alt={project.title} />
